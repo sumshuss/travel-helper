49b43feb73bf5eed3bc2fef04548a6048b838963
Summira Hussain <summira508411@gmail.com>
2022-11-01 15:38:24 +0000

    work to be pushes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { CssBaseline, Grid } from '@material-ui/core';
+import { Map, Header, PlaceDetails, List } from './components';
+import { getPlacesData } from './components/api';
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    getPlacesData()
+      .then((data) => {
+        console.log(data)
+        setPlaces(data);
+    })
+  }, [])
+
   return (
     <div className="App">
-        api work
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width : '100%' }}>
+              <Grid item xs={12} md={4}>
+                <List />
+              </Grid>
+              <Grid item xs={12} md={8}>
+                <Map />
+              </Grid>
+            </Grid>
+        </>
     </div>
   );
 }
